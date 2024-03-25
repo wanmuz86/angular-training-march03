bfdd15de2a7fa34c421adb4e6ede9d6be3b220ca
Wan Muzaffar <wanmuz86@gmail.com>
2024-03-25 14:38:05 +0800

    add message on button

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -12,15 +12,23 @@ export class ProductListComponent {
 // Imagine I get this from the internet
   welcome = 'Welcome to <strong>LazadaMall</strong>';
   number = 0;
+  message = "";
 
   incrementClick(){
+    this.message="";
     this.number++; // this, refer to the scope within the component
   }
   decrementClick(){
-    this.number--;
+    if (this.number == 0){
+      this.message = "Number cannot be less than 0";
+    }else {
+      this.number--;
+    }
+   
   }
   
   reset(){
+    this.message="";
     this.number = 0;
   }
 
